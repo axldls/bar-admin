f5f86d2e1451680e3ba1ef9f49a4d0e1e7a185b1
Axel De los santos <andresaxeldelossantos@mail.com>
2026-08-23 21:22:58 -0300

    ph

--- db.js
+++ db.js
@@ -2,10 +2,8 @@ require('dotenv').config();
 
 const { Pool } = require('pg');
 
-const RENDER_DATABASE_URL = 'postgresql://axel123z:elNxCbo4hL6eyMs7AF4MVOaKBNwnC2sO@dpg-da5nsjbm8hqs73db7ea0-a.oregon-postgres.render.com/bar_db_onjb';
-
 const pool = new Pool({
-	connectionString: process.env.DATABASE_URL || RENDER_DATABASE_URL,
+	connectionString: process.env.DATABASE_URL,
 	host: process.env.PGHOST,
 	port: process.env.PGPORT ? Number(process.env.PGPORT) : undefined,
 	database: process.env.PGDATABASE,
